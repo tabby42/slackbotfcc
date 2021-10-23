f16a2da8a8d72d27a211892f65c48834392cf42c
Irene Theiss <irene-rudel@gmx.at>
2021-10-23 15:43:54 +0200

    modify port variable

--- index.js
+++ index.js
@@ -61,8 +61,8 @@ app.command("/shout", async ({ command, ack, say }) => {
 });
 
 (async () => {
-    const port = 3000
+    const PORT = process.env.PORT || 5000
     // Start your app
-    await app.start(process.env.PORT || port);
-    console.log(`⚡️ Slack Bolt app is running on port ${port}!`);
+    await app.start(PORT);
+    console.log(`⚡️ Slack Bolt app is running on port ${PORT}!`);
 })();
